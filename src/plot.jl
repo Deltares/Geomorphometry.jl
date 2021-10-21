19dcecfe2cbdc78b2b4e6cdb4093dccfcbc083f2
Maarten Pronk <git@evetion.nl>
2021-10-21 22:40:10 +0200

    Added pssm plot.

---
+++ src/plot.jl
@@ -0,0 +1,30 @@
+# using RecipesBase
+using ImageFiltering
+using ImageCore
+
+"""
+```
+image = pssm(A; exaggeration, resolution)
+```
+Perceptually Shaded Slope Map by [Pingel, Thomas, and Keith Clarke. 2014].
+
+# Output
+- `image::Gray{T,2}` Grayscale image
+
+# Arguments
+- `A::Array{Real,2}` Input Array
+- `exaggeration::Real=2.3` Factor to exaggerate elevation
+- `resolution::Real=1.0` Resolution of cell size
+
+[Pingel, Thomas, and Keith Clarke. 2014]. ‘Perceptually Shaded Slope Maps for the Visualization of Digital Surface Models’. Cartographica: The International Journal for Geographic Information and Geovisualization 49 (4): 225–40. https://doi.org/10/ggnthv.]
+"""
+function pssm(A::Array{<:Real,2}; exaggeration=2.3, resolution=1.)
+    x, y = imgradients(A * exaggeration, ImageFiltering.sobel)
+    G = sqrt.(x.^2 .+ y.^2)
+
+    G /= resolution  # account for horizontal resolution
+
+    f = scaleminmax(0, 1)
+    clamped = f.(G)
+    Gray.(1 .- clamped)
+end
--- src/utils.jl
+++ src/utils.jl
@@ -2,9 +2,9 @@ using OffsetArrays
 using ImageFiltering
 
 """Apply the opening operation to `A` with window size `ω`."""
-function opening(A::Array{T,2}, ω::Integer) where T<:Real
-    A = mapwindow(minimum, A, (ω,ω))  # erosion
-    A = mapwindow(maximum, A, (ω,ω))  # dilation
+function opening(A::Array{T,2}, ω::Integer) where T <: Real
+    A = mapwindow(minimum, A, (ω, ω))  # erosion
+    A = mapwindow(maximum, A, (ω, ω))  # dilation
     A
 end
 
@@ -17,9 +17,9 @@ function circmask(n::Integer)
     return kern.parent
 end
 
-function opening_circ(A::Array{T,2}, ω::Integer) where T<:Real
-    m = circmask(ω>>1)
-    A = mapwindow(x->minimum(x[m]), A, (ω,ω))  # erosion
-    A = mapwindow(x->maximum(x[m]), A, (ω,ω))  # dilation
+function opening_circ(A::Array{T,2}, ω::Integer) where T <: Real
+    m = circmask(ω >> 1)
+    A = mapwindow(x -> minimum(x[m]), A, (ω, ω))  # erosion
+    A = mapwindow(x -> maximum(x[m]), A, (ω, ω))  # dilation
     A
 end
