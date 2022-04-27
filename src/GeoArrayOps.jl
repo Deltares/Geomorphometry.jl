9472444c59b1f00ebf54028e96f58894df8ab9fe
Maarten Pronk <git@evetion.nl>
2022-04-27 10:11:35 +0200

    Initial skewness filter.

--- src/GeoArrayOps.jl
+++ src/GeoArrayOps.jl
@@ -9,12 +9,14 @@ include("psf.jl")
 include("plot.jl")
 include("spread.jl")
 include("terrain.jl")
+include("skew.jl")
 
 export pmf, smf, psf
 export pssm
 export pitremoval
 export spread, spread2
 export roughness, TRI, TPI
+export skb
 
 # function __init__()
 #     A = rand(Float64, 3, 3)
@@ -29,16 +31,28 @@ export roughness, TRI, TPI
 # end
 
 precompile(pmf, (Matrix{Float64},))
+precompile(pmf, (Matrix{Float32},))
 precompile(psf, (Matrix{Float64},))
+precompile(psf, (Matrix{Float32},))
 precompile(pitremoval, (Matrix{Float64},))
+precompile(pitremoval, (Matrix{Float32},))
 precompile(smf, (Matrix{Float64},))
+precompile(smf, (Matrix{Float32},))
 precompile(pssm, (Matrix{Float64},))
+precompile(pssm, (Matrix{Float32},))
 precompile(spread, (Matrix{Float64}, Matrix{Float64}, Matrix{Float64}))
+precompile(spread, (Matrix{Float32}, Matrix{Float32}, Matrix{Float32}))
 precompile(spread2, (Matrix{Float64}, Matrix{Float64}, Matrix{Float64}))
+precompile(spread2, (Matrix{Float32}, Matrix{Float32}, Matrix{Float32}))
 precompile(spread, (Matrix{Float64}, Matrix{Float64}, Float64))
+precompile(spread, (Matrix{Float32}, Matrix{Float32}, Float32))
 precompile(spread, (Matrix{Float64}, Float64, Float64))
+precompile(spread, (Matrix{Float32}, Float64, Float64))
 precompile(roughness, (Matrix{Float64},))
+precompile(roughness, (Matrix{Float32},))
 precompile(TRI, (Matrix{Float64},))
+precompile(TRI, (Matrix{Float32},))
 precompile(TPI, (Matrix{Float64},))
+precompile(TPI, (Matrix{Float32},))
 
 end # module
---
+++ src/skew.jl
@@ -0,0 +1,34 @@
+# Skewness balancing as by Bartels (2006)
+using StatsBase
+"""
+```
+mask = skb(A, mean)
+mask = skb(A)
+```
+Applies skewness balancing by *Bartels e.a (2006)* [^bartels2006] to `A`.
+
+# Output
+- `mask::BitMatrix` Maximum allowable values
+
+Afterwards, one can retrieve the resulting mask for `A` by `A .<= B` or `flags .== 0.`.
+
+[^bartels2006]: Bartels, M., Hong Wei, and D.C. Mason. 2006. “DTM Generation from LIDAR Data Using Skewness Balancing.” In 18th International Conference on Pattern Recognition (ICPR’06), 1:566–69. https://doi.org/10/cwk4v2.
+"""
+function skb(A::AbstractMatrix{T}, mean::T) where {T<:Real}
+    I = sortperm(vec(A))
+    AA = A[I]
+    s = 1
+    for i ∈ length(A):-1:1
+        X = skewness(AA[begin:i], mean)
+        if X <= 0
+            s = i + 1
+            break
+        end
+    end
+    m = trues(size(A))
+    m[I[s+1:end]] .= false
+    return m
+end
+function skb(A::AbstractMatrix{T}) where {T<:Real}
+    return skb(A, mean(A))
+end
